d3dbbab69e1ca6ea55d60f59a2fb333872724d6b
Nimit Suwannagate <ethaizone@hotmail.com>
2012-07-11 17:19:30 +0700

    Add ci_config.php

---
+++ ci_config.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * Example description
+ */
+$config['example'] = '';
+
+/* End of file ${nameAndExt} */
+/* Location: ./application/config/${nameAndExt} */
